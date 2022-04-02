b755a48a6cb4306bfcf2c4e8bfa0ab5898ebf8e3
tanyaleepr <tanyaleepr@gmail.com>
2022-04-01 19:03:53 -0500

    finished script for horoscope

--- script.js
+++ script.js
@@ -1,87 +1,87 @@
-document.getElementById('#aries').onclick = ariesOne;
-document.getElementById('#taurus').onclick = taurusTwo;
-document.getElementById('#gemini').onclick = geminisThree;
-document.getElementById('#cancer').onclick = cancerFour;
-document.getElementById('#leo').onclick = leoFive;
-document.getElementById('#virgo').onclick = virgoSix;
-document.getElementById('#libra').onclick = libraSeven;
-document.getElementById('#scorpio').onclick = scorpioEight;
-document.getElementById('#sagittarius').onclick = sagittariusNine;
-document.getElementById('#capricorn').onclick = capricornTen;
-document.getElementById('#aquarius').onclick = aquariusEleven;
-document.getElementById('#pisces').onclick = piscesTwelve;
+document.getElementById('aries').onclick = ariesOne;
+document.getElementById('taurus').onclick = taurusTwo;
+document.getElementById('gemini').onclick = geminisThree;
+document.getElementById('cancer').onclick = cancerFour;
+document.getElementById('leo').onclick = leoFive;
+document.getElementById('virgo').onclick = virgoSix;
+document.getElementById('libra').onclick = libraSeven;
+document.getElementById('scorpio').onclick = scorpioEight;
+document.getElementById('sagittarius').onclick = sagittariusNine;
+document.getElementById('capricorn').onclick = capricornTen;
+document.getElementById('aquarius').onclick = aquariusEleven;
+document.getElementById('pisces').onclick = piscesTwelve;
 
 
 
 function ariesOne() {
-  document.body.style.backgroundImage =
+  document.querySelector('.first').style.backgroundImage =
   "url('https://media.giphy.com/media/G6ojXggFcXWCs/giphy.gif')";
 }
 // <!--Gretchen Wieners-->
 
 function taurusTwo() {
-  document.body.style.backgroundImage =
+    document.querySelector('.first').style.backgroundImage =
   "url('https://media.giphy.com/media/YWlN1XXSNEDb1BMqRL/giphy.gif')";
 }
 // <!--Cady Heron-->
 
 function geminisThree() {
-  document.body.style.backgroundImage =
+    document.querySelector('.first').style.backgroundImage =
   "url('https://media.giphy.com/media/xT9KVLMzuagLc6b36U/giphy.gif')";
 }
 // <!--Karen Smith-->
 
 function cancerFour() {
-  document.body.style.backgroundImage =
-  "url('https://media.giphy.com/media/JZFzZ3Mxpls9q/giphy.gif')";
+    document.querySelector('.first').style.backgroundImage =
+  "url('https://media.giphy.com/media/x6dkx2CQLcRWifJLnN/giphy.gif')";
 } 
 // <!--Mrs. George-->
 
 function leoFive() {
-  document.body.style.backgroundImage =
+    document.querySelector('.first').style.backgroundImage =
   "url('https://media.giphy.com/media/3o7aTLOXmU5mz0KXIY/giphy.gif')";
 }
 // <!--Aaron Samuels-->
 
 function virgoSix() {
-  document.body.style.backgroundImage =
-  "url('https://media.giphy.com/media/CMc604oNR1dzG/giphy.gif')";
+    document.querySelector('.first').style.backgroundImage =
+  "url('https://media.giphy.com/media/3o7aTlAhsx5wNVh4Dm/giphy.gif')";
 }
 // <!--Regina George-->
 
 function libraSeven() {
-  document.body.style.backgroundImage =
-  "url('https://media.giphy.com/media/3o7aTDjjoVh1Z9rD2g/giphy.gif')";
+    document.querySelector('.first').style.backgroundImage =
+  "url('https://media.giphy.com/media/xT9KVprQnsZdsLlMrK/giphy.gif')";
 }
-// <!--Coach Carr-->
+// <!--Dean-->
 
 function scorpioEight() {
-  document.body.style.backgroundImage =
-  "url('https://media.giphy.com/media/1lwmbLpLSRGRQg4mqu/giphy.gif')";
+    document.querySelector('.first').style.backgroundImage =
+  "url('https://media.giphy.com/media/MXQpR6KTtm5Bi04YS3/giphy.gif')";
 }
 // <!--Janis Ian-->
 
 function sagittariusNine() {
-  document.body.style.backgroundImage =
-  "url('https://media.giphy.com/media/l2YWtqm1NkjAFSIKc/giphy.gif')";
+    document.querySelector('.first').style.backgroundImage =
+  "url('https://media.giphy.com/media/QXCzuhFF3wAlpD6ScL/giphy.gif')";
 }
 // <!--Damian-->
 
 function capricornTen() {
-  document.body.style.backgroundImage =
+    document.querySelector('.first').style.backgroundImage =
   "url('https://media.giphy.com/media/3o7aTLkyh3yAG6DEuQ/giphy.gif')";
 }
 // <!--Ms. Norbury-->
 
 function aquariusEleven() {
-  document.body.style.backgroundImage =
+    document.querySelector('.first').style.backgroundImage =
   "url('https://media.giphy.com/media/eaIpScHwBsA5a/giphy.gif')";
 }
 
 // <!--Joan the Secretary-->
 
 function piscesTwelve() {
-  document.body.style.backgroundImage =
-  "url('https://media.giphy.com/media/6ppl7BehlBlUk/giphy.gif')";
+    document.querySelector('.first').style.backgroundImage =
+  "url('https://media.giphy.com/media/xT9Iguix3T9EU9Y5Ik/giphy.gif')";
 }
 // <!--Kevin Gnapoor-->
